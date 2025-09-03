101a1a064c60066608db9bd3dc59616e144eb72d
Liam Fuller <lpfuller28@wabash.edu>
2025-09-03 10:53:07 -0400

    Add: TicTacToe

--- TicTacToe/TicTacToe1.py
+++ TicTacToe/TicTacToe1.py
@@ -21,7 +21,23 @@ class GameBoard:
 
     def checkwin(self) -> int:
 
-        return 0
+        for line in WIN_LINES:
+            vals = [self.entries[r][c] for r,c in line]
+            if vals == [1,1,1]:
+                self.state = 1
+                return 1
+            if vals == [2,2,2]:
+                self.state = 2
+                return 2
+        if any(0 in row for row in self.entries):
+            self.state = 0
+            return 0
+        self.state = 3
+        return 3
+
+
+
 
 gb = GameBoard()
 gb.print()
+
